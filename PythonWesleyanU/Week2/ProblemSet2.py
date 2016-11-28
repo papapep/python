dd6bba1039b2a0a571c147d2700a26349ad8c091
papapep <papapep@gmx.com>
2016-11-28 09:10:14 +0100

    All of the 2nd weed exercises passed

--- PythonWesleyanU/Week2/ProblemSet2.py
+++ PythonWesleyanU/Week2/ProblemSet2.py
@@ -299,7 +299,9 @@ def problem2_7():
     side1=float(input("enter length of side one:"))
     side2=float(input("enter length of side two:"))
     side3=float(input("enter length of side three:"))
-    print("Area of a triangle with sides",side1,side2,side3,"is",(.5*(side1+side2+side3)))
+    s = .5*(side1+side2+side3)
+    area = (s*(s-side1)*(s-side2)*(s-side3))**.5
+    print("Area of a triangle with sides",side1,side2,side3,"is",area)
     
 #%%
 """ 
